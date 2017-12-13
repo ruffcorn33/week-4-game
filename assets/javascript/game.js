855f28e2c65e23c46fd69feecdf69c9c9854ee56
Craig Christensen <ruffcorn@gmail.com>
2017-12-13 16:08:51 -0600

    Changed layout, added sounds, added win/lose message.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,63 +1,91 @@
 $(document).ready(function(){
 
-  // declare variables
+  // declare global variables
 
-  var rdmNumber;
+  var rdmNumber = 0;
   var guessTotal = 0;
   var gemValues = new Array();
-  var message;
   var winCnt = 0;
   var lossCnt = 0;
   var isEnd = false;
+  var isWin = false;
 
-
-  getRdmNumber = function(min, max){
+  getRdmNumber = function(min, max){ // get a random number between 19 and 120px
     return Math.floor(Math.random() * (max - min) + min);
   }
 
-  fillGemValues = function(x) {
+  fillGemValues = function(x) { // if gem value is not already in array, push it to array
     if (jQuery.inArray( y, gemValues ) === -1){
-      //debugger;
       gemValues.push(x);
     }
   }
 
-  start = function() {
+  start = function() { // init variables, get new random goal number, get hidden values for gems
+    debugger;
     isEnd = false;
+    isWin = false;
     message = "";
     guessTotal = 0;
-    document.getElementById('guessTot').innerHTML = guessTotal;
-    document.getElementById('winLossMsg').innerHTML = message;
-    document.getElementById('rdmNum').innerHTML = rdmNumber = getRdmNumber(19, 121);
-    var i = 0;
-    while (gemValues.length < 4) {
-      y = Math.ceil(Math.random() * 12);
-      fillGemValues(y);
+    gemValues = [];
+    // document.getElementById('guessTot').innerHTML = guessTotal;
+    $("#guessTot").text(guessTotal);
+    $("#rdmNum").text(rdmNumber = getRdmNumber(19, 121));
+    $('#crystalsRow').show();
+    $('#messageRow').hide();
+    while (gemValues.length < 4) {  // generate random gem values between 1 and 12
+      fillGemValues(y = Math.ceil(Math.random() * 12));
     }
-    console.log(gemValues);
   }
 
-  $(".image").on("click", function() {
-    document.getElementById('guessTot').innerHTML = guessTotal = (guessTotal + gemValues[$(this).attr('value')]);
-    var clinkAudio = document.getElementById("clink");
+  $("#crystalsRow").mouseenter(function() {
+    var hoverAudio = $("#imgBoxHover")[0];
+    hoverAudio.play();
+  });
+
+  $("#crystalsRow").mouseleave(function() {
+    var hoverAudio = $("#imgBoxHover")[0];
+    hoverAudio.pause();
+  });
+
+  $(".image").on("click", function() { // click handler for gems
+    // accumulate guesses
+    guessTotal = (guessTotal + gemValues[$(this).attr('value')]);
+    $('#guessTot').text(guessTotal);
+    var clinkAudio = document.getElementById("ping");
     clinkAudio.play();
+    // test for win or loss
     if (guessTotal === rdmNumber) {
       winCnt++;
-      document.getElementById('winScore').innerHTML = winCnt;
+      $('#winScore').text(winCnt);
       message = "You won!";
-      document.getElementById('winLossMsg').innerHTML = message;
       isEnd = true;
+      isWin = true;
     }
     else if (guessTotal > rdmNumber) {
       lossCnt++;
-      document.getElementById('lossScore').innerHTML = lossCnt;
+      $('#lossScore').text(lossCnt);
       message = "You lost!";
-      document.getElementById('winLossMsg').innerHTML = message;
       isEnd = true;
     }
-
+    // if win or loss, restart
     if (isEnd) {
-      start();
+      $('#crystalsRow').hide();
+      $('#messageRow').show();
+      if (isWin) {
+        var successAudio = document.getElementById("success");
+        successAudio.play();
+        $('#messageRow').attr('color','green');
+        $('#messageRow').html("<span id='messageSpan' class='text-success'>You won!</span>")
+        // $('#messageRow').html("<span id='messageSpan' class='text-success fade out'>You won!</span>")
+      }
+      else {
+        var breakAudio = document.getElementById("break");
+        breakAudio.play();
+        $('#messageRow').attr('color','red');
+        $('#messageRow').html("<span id='messageSpan' class='text-danger'>You lost!</span>");
+        // $('#messageRow').html("<span id='messageSpan' class='text-danger fade out'>You lost!</span>");
+      }
+      setTimeout(function(){ start(); }, 3000);
     }
 
   });
